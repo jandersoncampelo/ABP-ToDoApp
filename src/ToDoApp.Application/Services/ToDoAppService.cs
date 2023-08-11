1ff17251229a9bf671fc5971e40fcb913e00ead3
Janderson Campelo <janderson.souza@becomex.com.br>
2023-08-11 17:01:02 -0300

    Camada Application - Criação da Service

---
+++ src/ToDoApp.Application/Services/ToDoAppService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToDoApp.Entities;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Domain.Repositories;
+
+namespace ToDoApp.Services
+{
+    public class ToDoAppService : ApplicationService, IToDoAppService
+    {
+        private readonly IRepository<ToDoItem, Guid> _repository;
+
+        public ToDoAppService(IRepository<ToDoItem, Guid> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<ToDoItemDto> CreateAsync(string text)
+        {
+            var toDoItem = await _repository.InsertAsync(
+                new ToDoItem
+                {
+                    Text = text
+                },
+                autoSave: true
+            );
+
+            return new ToDoItemDto
+            {
+                Id = toDoItem.Id,
+                Text = toDoItem.Text
+            };
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            await _repository.DeleteAsync(id);
+        }
+
+        public async Task<List<ToDoItemDto>> GetListAsync()
+        {
+            var items = await _repository.GetListAsync();
+            return items.Select(item => new ToDoItemDto
+            {
+                Id = item.Id,
+                Text = item.Text
+            }).ToList();
+        }
+    }
+}
