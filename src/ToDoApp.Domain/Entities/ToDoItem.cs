2279769b7e600429b5f581b31ba8243acd3aae69
Janderson Campelo <janderson.souza@becomex.com.br>
2023-08-11 16:59:53 -0300

    Camada Domain - Criação da Entidade

---
+++ src/ToDoApp.Domain/Entities/ToDoItem.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Volo.Abp.Domain.Entities;
+
+namespace ToDoApp.Entities
+{
+    public class ToDoItem : BasicAggregateRoot<Guid>
+    {
+        public string Text { get; set; }
+    }
+}
