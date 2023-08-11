79598878a7280d879838c3f608a693528ecc7770
Janderson Campelo <janderson.souza@becomex.com.br>
2023-08-11 17:01:24 -0300

    Front Blazor - Criação da Tela

--- src/ToDoApp.Blazor/Pages/Index.razor.cs
+++ src/ToDoApp.Blazor/Pages/Index.razor.cs
@@ -1,6 +1,35 @@
-﻿namespace ToDoApp.Blazor.Pages;
+﻿using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ToDoApp.Blazor.Pages;
 
 public partial class Index
 {
+    [Inject]
+    private IToDoAppService ToDoAppService { get; set; }
 
+    private List<ToDoItemDto> ToDoItems { get; set; } = new List<ToDoItemDto>();
+    private string NewToDoText { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        ToDoItems = await ToDoAppService.GetListAsync();
+    }
+
+    private async Task Create()
+    {
+        var result = await ToDoAppService.CreateAsync(NewToDoText);
+        ToDoItems.Add(result);
+
+        NewToDoText = string.Empty;
+    }
+
+    private async Task Delete(ToDoItemDto todoItem)
+    {
+        await ToDoAppService.DeleteAsync(todoItem.Id);
+        //await Notify.Info("Deleted the To-Do item successfully.");
+
+        ToDoItems.Remove(todoItem);
+    }
 }
